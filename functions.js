f1e820c95e570851ac5e2dab9bbbebfe59e6ab66
Srfreeny <srfreeny@comcast.net>
2020-07-20 19:57:37 -0500

    made the age generator function and the nuggetizer function

---
+++ functions.js
@@ -0,0 +1,25 @@
+console.log("functions")
+
+const nuggetizer = (animal) => {
+    return `processed ${animal}`
+}
+
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('student'));
+
+const numFunction = (number) => {
+    return number + 42
+}
+
+console.log(numFunction(33));
+console.log(numFunction(9023));
+console.log(numFunction(-42));
+
+
+const ageGenerator = (age) => {
+    return 2099 - age
+}
+
+console.log(ageGenerator(1993));
